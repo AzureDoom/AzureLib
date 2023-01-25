546dbac57b2d3b1dc5b9d6b8de30f24bdfac4595
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-01-24 19:37:16 -0500

    Fixes the entity packet

--- Fabric/src/main/java/mod/azure/azurelib/network/packet/EntityPacket.java
+++ Fabric/src/main/java/mod/azure/azurelib/network/packet/EntityPacket.java
@@ -6,12 +6,13 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 
 public class EntityPacket {
 
-	public static Packet<?> createPacket(Entity entity) {
+	public static Packet<ClientGamePacketListener> createPacket(Entity entity) {
 		FriendlyByteBuf buf = createBuffer();
 		buf.writeVarInt(BuiltInRegistries.ENTITY_TYPE.getId(entity.getType()));
 		buf.writeUUID(entity.getUUID());
@@ -20,7 +21,7 @@ public class EntityPacket {
 		buf.writeDouble(entity.getY());
 		buf.writeDouble(entity.getZ());
 		buf.writeByte(Mth.floor(entity.getXRot() * 256.0F / 360.0F));
-		buf.writeByte(Mth.floor(entity.getYRot()  * 256.0F / 360.0F));
+		buf.writeByte(Mth.floor(entity.getYRot() * 256.0F / 360.0F));
 		buf.writeFloat(entity.getXRot());
 		buf.writeFloat(entity.getYRot());
 		return ServerPlayNetworking.createS2CPacket(AzureLibNetwork.CUSTOM_ENTITY_ID, buf);
