0f5b3f8d5fc3cfaf1b9ffc056eb149320e23fb10
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-06-06 03:52:46 -0400

    Forge branch updated for latest 1.19.4 changes in order for 1.20 update on the 7th

--- Forge/src/main/java/mod/azure/azurelib/util/AzureLibUtil.java
+++ Forge/src/main/java/mod/azure/azurelib/util/AzureLibUtil.java
@@ -1,16 +1,16 @@
 package mod.azure.azurelib.util;
 
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import mod.azure.azurelib.constant.DataTickets;
 import mod.azure.azurelib.core.animatable.GeoAnimatable;
-import mod.azure.azurelib.core.animation.Animation;
-import mod.azure.azurelib.core.animation.EasingType;
 import mod.azure.azurelib.core.animatable.instance.AnimatableInstanceCache;
 import mod.azure.azurelib.core.animatable.instance.InstancedAnimatableInstanceCache;
 import mod.azure.azurelib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import mod.azure.azurelib.core.animation.Animation;
+import mod.azure.azurelib.core.animation.EasingType;
 import mod.azure.azurelib.loading.object.BakedModelFactory;
 import mod.azure.azurelib.network.SerializableDataTicket;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 /**
  * Helper class for various AzureLib-specific functions.
@@ -18,27 +18,36 @@ import mod.azure.azurelib.network.SerializableDataTicket;
 public final class AzureLibUtil {
 	/**
 	 * Creates a new AnimatableInstanceCache for the given animatable object
+	 * 
 	 * @param animatable The animatable object
 	 */
 	public static AnimatableInstanceCache createInstanceCache(GeoAnimatable animatable) {
-		return createInstanceCache(animatable, !(animatable instanceof Entity) && !(animatable instanceof BlockEntity));
+		AnimatableInstanceCache cache = animatable.animatableCacheOverride();
+
+		return cache != null ? cache : createInstanceCache(animatable, !(animatable instanceof Entity) && !(animatable instanceof BlockEntity));
 	}
 
 	/**
 	 * Creates a new AnimatableInstanceCache for the given animatable object. <br>
 	 * Recommended to use {@link AzureLibUtil#createInstanceCache(GeoAnimatable)} unless you know what you're doing.
-	 * @param animatable The animatable object
+	 * 
+	 * @param animatable      The animatable object
 	 * @param singletonObject Whether the object is a singleton/flyweight object, and uses ints to differentiate animatable instances
 	 */
 	public static AnimatableInstanceCache createInstanceCache(GeoAnimatable animatable, boolean singletonObject) {
+		AnimatableInstanceCache cache = animatable.animatableCacheOverride();
+
+		if (cache != null)
+			return cache;
+		
 		return singletonObject ? new SingletonAnimatableInstanceCache(animatable) : new InstancedAnimatableInstanceCache(animatable);
 	}
 
 	/**
-	 * Register a custom {@link mod.azure.azurelib.core.animation.Animation.LoopType} with AzureLib,
-	 * allowing for dynamic handling of post-animation looping.<br>
+	 * Register a custom {@link mod.azure.azurelib.core.animation.Animation.LoopType} with AzureLib, allowing for dynamic handling of post-animation looping.<br>
 	 * <b><u>MUST be called during mod construct</u></b><br>
-	 * @param name The name of the {@code LoopType} handler
+	 * 
+	 * @param name     The name of the {@code LoopType} handler
 	 * @param loopType The {@code LoopType} implementation to use for the given name
 	 */
 	synchronized public static Animation.LoopType addCustomLoopType(String name, Animation.LoopType loopType) {
@@ -46,10 +55,10 @@ public final class AzureLibUtil {
 	}
 
 	/**
-	 * Register a custom {@link mod.azure.azurelib.core.animation.EasingType} with AzureLib,
-	 * allowing for dynamic handling of animation transitions and curves.<br>
+	 * Register a custom {@link mod.azure.azurelib.core.animation.EasingType} with AzureLib, allowing for dynamic handling of animation transitions and curves.<br>
 	 * <b><u>MUST be called during mod construct</u></b><br>
-	 * @param name The name of the {@code EasingType} handler
+	 * 
+	 * @param name       The name of the {@code EasingType} handler
 	 * @param easingType The {@code EasingType} implementation to use for the given name
 	 */
 	synchronized public static EasingType addCustomEasingType(String name, EasingType easingType) {
@@ -57,11 +66,11 @@ public final class AzureLibUtil {
 	}
 
 	/**
-	 * Register a custom {@link mod.azure.azurelib.loading.object.BakedModelFactory} with AzureLib,
-	 * allowing for dynamic handling of geo model loading.<br>
+	 * Register a custom {@link mod.azure.azurelib.loading.object.BakedModelFactory} with AzureLib, allowing for dynamic handling of geo model loading.<br>
 	 * <b><u>MUST be called during mod construct</u></b><br>
+	 * 
 	 * @param namespace The namespace (modid) to register the factory for
-	 * @param factory The factory responsible for model loading under the given namespace
+	 * @param factory   The factory responsible for model loading under the given namespace
 	 */
 	synchronized public static void addCustomBakedModelFactory(String namespace, BakedModelFactory factory) {
 		BakedModelFactory.register(namespace, factory);
@@ -70,6 +79,7 @@ public final class AzureLibUtil {
 	/**
 	 * Register a custom {@link SerializableDataTicket} with AzureLib for handling custom data transmission.<br>
 	 * NOTE: You do not need to register non-serializable {@link mod.azure.azurelib.core.object.DataTicket DataTickets}.
+	 * 
 	 * @param dataTicket The SerializableDataTicket to register
 	 * @return The dataTicket you passed in
 	 */
