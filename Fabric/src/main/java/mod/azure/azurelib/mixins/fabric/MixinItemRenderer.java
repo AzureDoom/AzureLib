d0c1e5c6db993bbe5a6cd088dd85e86cacc116c1
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-06-16 20:01:45 -0400

    Port Headed crash fix
    This is just a fix for AzureLib

--- Fabric/src/main/java/mod/azure/azurelib/mixins/fabric/MixinItemRenderer.java
+++ Fabric/src/main/java/mod/azure/azurelib/mixins/fabric/MixinItemRenderer.java
@@ -7,6 +7,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
+import mod.azure.azurelib.animatable.GeoItem;
 import mod.azure.azurelib.animatable.client.RenderProvider;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.ItemRenderer;
@@ -20,9 +21,8 @@ import net.minecraft.world.item.ItemStack;
 @Mixin(ItemRenderer.class)
 public class MixinItemRenderer {
 	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/BlockEntityWithoutLevelRenderer;renderByItem(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V"), cancellable = true)
-	public void itemModelHook(ItemStack itemStack, ItemDisplayContext transformType, boolean bl, PoseStack poseStack,
-			MultiBufferSource multiBufferSource, int i, int j, BakedModel bakedModel, CallbackInfo ci) {
-		RenderProvider.of(itemStack).getCustomRenderer().renderByItem(itemStack, transformType, poseStack,
-				multiBufferSource, i, j);
+	public void itemModelHook(ItemStack itemStack, ItemDisplayContext transformType, boolean bl, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, BakedModel bakedModel, CallbackInfo ci) {
+		if (itemStack.getItem() instanceof GeoItem)
+			RenderProvider.of(itemStack).getCustomRenderer().renderByItem(itemStack, transformType, poseStack, multiBufferSource, i, j);
 	}
 }
