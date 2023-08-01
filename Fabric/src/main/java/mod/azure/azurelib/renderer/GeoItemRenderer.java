3858f89626acb59824103407ccf9b072c6765578
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-08-01 00:33:38 -0400

    Revert Item Lighting Tweak

--- Fabric/src/main/java/mod/azure/azurelib/renderer/GeoItemRenderer.java
+++ Fabric/src/main/java/mod/azure/azurelib/renderer/GeoItemRenderer.java
@@ -186,7 +186,7 @@ public class GeoItemRenderer<T extends Item & GeoAnimatable> extends BlockEntity
 		VertexConsumer buffer = ItemRenderer.getFoilBufferDirect(bufferSource, renderType, true, this.currentItemStack != null && this.currentItemStack.hasFoil());
 
 		poseStack.pushPose();
-		Lighting.setupForEntityInInventory();
+		Lighting.setupForFlatItems();
 		defaultRender(poseStack, this.animatable, defaultBufferSource, renderType, buffer,
 				0, Minecraft.getInstance().getFrameTime(), packedLight);
 		defaultBufferSource.endBatch();
