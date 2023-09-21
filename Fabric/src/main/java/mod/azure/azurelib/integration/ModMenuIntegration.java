1b064f32ce425eb535f5a2f1ecc1e71e612d75d5
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-09-21 13:42:34 -0400

    Forgot to rename Mod Menu support for configs

--- Fabric/src/main/java/mod/azure/azurelib/integration/ModMenuIntegration.java
+++ Fabric/src/main/java/mod/azure/azurelib/integration/ModMenuIntegration.java
@@ -1,25 +1,34 @@
 package mod.azure.azurelib.integration;
 
-public class ModMenuIntegration { // implements ModMenuApi {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-//    @Override
-//    public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
-//        Map<String, ConfigScreenFactory<?>> map = new HashMap<>();
-//        Map<String, List<ConfigHolder<?>>> byGroup = ConfigHolder.getConfigGroupingByGroup();
-//        for (Map.Entry<String, List<ConfigHolder<?>>> entry : byGroup.entrySet()) {
-//            String group = entry.getKey();
-//            List<ConfigHolder<?>> configHolders = entry.getValue();
-//            ConfigScreenFactory<?> factory = parent -> {
-//                int i = configHolders.size();
-//                if (i > 1) {
-//                    return AzureLibMod.getConfigScreenByGroup(configHolders, group, parent);
-//                } else if (i == 1) {
-//                    return AzureLibMod.getConfigScreenForHolder(configHolders.get(0), parent);
-//                }
-//                return null;
-//            };
-//            map.put(group, factory);
-//        }
-//        return map;
-//    }
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+import mod.azure.azurelib.AzureLibMod;
+import mod.azure.azurelib.config.ConfigHolder;
+
+public class ModMenuIntegration implements ModMenuApi {
+
+    @Override
+    public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
+        Map<String, ConfigScreenFactory<?>> map = new HashMap<>();
+        Map<String, List<ConfigHolder<?>>> byGroup = ConfigHolder.getConfigGroupingByGroup();
+        for (Map.Entry<String, List<ConfigHolder<?>>> entry : byGroup.entrySet()) {
+            var group = entry.getKey();
+            List<ConfigHolder<?>> configHolders = entry.getValue();
+            ConfigScreenFactory<?> factory = parent -> {
+                var i = configHolders.size();
+                if (i > 1) 
+                    return AzureLibMod.getConfigScreenByGroup(configHolders, group, parent);
+                else if (i == 1) 
+                    return AzureLibMod.getConfigScreenForHolder(configHolders.get(0), parent);
+                return null;
+            };
+            map.put(group, factory);
+        }
+        return map;
+    }
 }
