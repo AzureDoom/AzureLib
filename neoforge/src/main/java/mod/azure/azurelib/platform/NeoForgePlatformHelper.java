dad6430cc40540568fbb81843d0c26ba53913ab9
Boston Vanseghi <bvanseg@gmail.com>
2023-10-27 10:04:24 -0400

    Added NeoForgePlatformHelper.java.
    Signed-off-by: Boston Vanseghi <bvanseg@gmail.com>

---
+++ neoforge/src/main/java/mod/azure/azurelib/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package mod.azure.azurelib.platform;
+
+import mod.azure.azurelib.platform.services.IPlatformHelper;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "Forge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
