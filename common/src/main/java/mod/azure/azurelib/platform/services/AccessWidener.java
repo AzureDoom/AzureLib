314e9b487f43a123a4a4ee2f9501f74047fe9049
Boston Vanseghi <bvanseg@gmail.com>
2023-10-27 13:30:52 -0400

    Moved AutoGlowingTexture.java static fields to access wideners.
    Signed-off-by: Boston Vanseghi <bvanseg@gmail.com>

--- common/src/main/java/mod/azure/azurelib/platform/services/AccessWidener.java
+++ common/src/main/java/mod/azure/azurelib/platform/services/AccessWidener.java
@@ -1,9 +1,12 @@
 package mod.azure.azurelib.platform.services;
 
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 
 import java.util.Map;
+import java.util.function.Function;
 
 public interface AccessWidener {
 
@@ -13,5 +16,7 @@ public interface AccessWidener {
      * @deprecated Minecraft.class exposes a getter for a single entity renderer by entity type. This is here for parity reasons.
      */
     @Deprecated
-    public Map<EntityType<?>, EntityRenderer<?>> getEntityRenderers();
+    Map<EntityType<?>, EntityRenderer<?>> getEntityRenderers();
+
+    Function<ResourceLocation, RenderType> getRenderTypeFunction();
 }
--- fabric/src/main/java/mod/azure/azurelib/platform/FabricAccessWidener.java
+++ fabric/src/main/java/mod/azure/azurelib/platform/FabricAccessWidener.java
@@ -1,15 +1,45 @@
 package mod.azure.azurelib.platform;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.VertexFormat;
 import mod.azure.azurelib.platform.services.AccessWidener;
+import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.RenderStateShard;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 
 import java.util.Map;
+import java.util.function.Function;
 
 public class FabricAccessWidener implements AccessWidener {
+    private static final RenderStateShard.ShaderStateShard SHADER_STATE = new RenderStateShard.ShaderStateShard(GameRenderer::getRendertypeEntityTranslucentEmissiveShader);
+    private static final RenderStateShard.TransparencyStateShard TRANSPARENCY_STATE = new RenderStateShard.TransparencyStateShard("translucent_transparency", () -> {
+        RenderSystem.enableBlend();
+        RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+    }, () -> {
+        RenderSystem.disableBlend();
+        RenderSystem.defaultBlendFunc();
+    });
+    private static final RenderStateShard.WriteMaskStateShard WRITE_MASK = new RenderStateShard.WriteMaskStateShard(true, true);
+    private static final Function<ResourceLocation, RenderType> RENDER_TYPE_FUNCTION = Util.memoize(texture -> {
+        RenderStateShard.TextureStateShard textureState = new RenderStateShard.TextureStateShard(texture, false, false);
+
+        return RenderType.create("geo_glowing_layer", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.QUADS, 256, false, true, RenderType.CompositeState.builder().setShaderState(SHADER_STATE).setTextureState(textureState).setTransparencyState(TRANSPARENCY_STATE).setWriteMaskState(WRITE_MASK).createCompositeState(false));
+    });
+
     @Override
     public Map<EntityType<?>, EntityRenderer<?>> getEntityRenderers() {
         return Minecraft.getInstance().getEntityRenderDispatcher().renderers;
+    }
+
+    @Override
+    public Function<ResourceLocation, RenderType> getRenderTypeFunction() {
+        return RENDER_TYPE_FUNCTION;
     }
 }
