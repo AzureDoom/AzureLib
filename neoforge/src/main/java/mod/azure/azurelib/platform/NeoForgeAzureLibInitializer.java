a7855fb0e2f82a71b2ac6d4d960ce0dd9dc02936
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-10-31 13:19:27 -0400

    Update for correct usage

--- neoforge/src/main/java/mod/azure/azurelib/platform/NeoForgeAzureLibInitializer.java
+++ neoforge/src/main/java/mod/azure/azurelib/platform/NeoForgeAzureLibInitializer.java
@@ -3,12 +3,14 @@ package mod.azure.azurelib.platform;
 import mod.azure.azurelib.cache.AzureLibCache;
 import mod.azure.azurelib.platform.services.AzureLibInitializer;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.DistExecutor;
+import net.neoforged.fml.loading.FMLEnvironment;
 
 public class NeoForgeAzureLibInitializer implements AzureLibInitializer {
     @Override
     public void initialize() {
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> AzureLibCache::registerReloadListener);
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            AzureLibCache.registerReloadListener();
+        }
         Services.NETWORK.registerClientReceiverPackets();
     }
 }
