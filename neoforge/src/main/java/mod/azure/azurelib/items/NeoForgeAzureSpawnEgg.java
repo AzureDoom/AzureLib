c898ef8a5c9a0f4a8e11c621ff8bc5fb3843ffc5
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-10-30 20:30:00 -0400

    ForgeSpawnEggItem -> DeferredSpawnEggItem

--- neoforge/src/main/java/mod/azure/azurelib/items/NeoForgeAzureSpawnEgg.java
+++ neoforge/src/main/java/mod/azure/azurelib/items/NeoForgeAzureSpawnEgg.java
@@ -1,20 +1,20 @@
 package mod.azure.azurelib.items;
 
-import java.util.function.Supplier;
-
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.common.ForgeSpawnEggItem;
+import net.neoforged.neoforge.common.DeferredSpawnEggItem;
 
-public class NeoForgeAzureSpawnEgg extends ForgeSpawnEggItem {
+import java.util.function.Supplier;
 
-	public NeoForgeAzureSpawnEgg(Supplier<? extends EntityType<? extends Mob>> type, int primaryColor, int secondaryColor) {
-		super(type, primaryColor, secondaryColor, new Item.Properties().stacksTo(64));
-	}
+public class NeoForgeAzureSpawnEgg extends DeferredSpawnEggItem {
 
-	public NeoForgeAzureSpawnEgg(EntityType<? extends Mob> type, int primaryColor, int secondaryColor) {
-		super(() -> type, primaryColor, secondaryColor, new Item.Properties().stacksTo(64));
-	}
+    public NeoForgeAzureSpawnEgg(Supplier<? extends EntityType<? extends Mob>> type, int primaryColor, int secondaryColor) {
+        super(type, primaryColor, secondaryColor, new Item.Properties().stacksTo(64));
+    }
+
+    public NeoForgeAzureSpawnEgg(EntityType<? extends Mob> type, int primaryColor, int secondaryColor) {
+        super(() -> type, primaryColor, secondaryColor, new Item.Properties().stacksTo(64));
+    }
 
 }
