1a6420f330f01c9dbe99a7296eb7ad549e3904fd
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-10-31 14:51:21 -0400

    Backport change, doesn't require a new build at this time as both do the same, this is just preferred way of doing it.

--- neoforge/src/main/java/mod/azure/azurelib/platform/NeoForgeAzureLibInitializer.java
+++ neoforge/src/main/java/mod/azure/azurelib/platform/NeoForgeAzureLibInitializer.java
@@ -4,11 +4,14 @@ import mod.azure.azurelib.cache.AzureLibCache;
 import mod.azure.azurelib.platform.services.AzureLibInitializer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 public class NeoForgeAzureLibInitializer implements AzureLibInitializer {
     @Override
     public void initialize() {
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> AzureLibCache::registerReloadListener);
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            AzureLibCache.registerReloadListener();
+        }
         Services.NETWORK.registerClientReceiverPackets();
     }
 }
