bfdd25f99cf8e0a0ad5468bcce44b1bc28725a64
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-11-07 14:45:09 -0500

    Format/Add Doc

--- common/src/main/java/mod/azure/azurelib/common/api/common/interfaces/Growable.java
+++ common/src/main/java/mod/azure/azurelib/common/api/common/interfaces/Growable.java
@@ -1,42 +1,46 @@
 package mod.azure.azurelib.common.api.common.interfaces;
 
-import static java.lang.Math.min;
-
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 
+import static java.lang.Math.min;
+
+/**
+ * Interface of having entities grow into entity based a growth value.
+ * @author Boston Vanseghi
+ */
 public interface Growable {
-	float getGrowth();
+    float getGrowth();
 
-	void setGrowth(float growth);
+    void setGrowth(float growth);
 
-	float getMaxGrowth();
+    float getMaxGrowth();
 
-	default void grow(LivingEntity entity, float amount) {
-		setGrowth(min(getGrowth() + amount, getMaxGrowth()));
-		if (getGrowth() >= getMaxGrowth())
-			growUp(entity);
-	}
+    default void grow(LivingEntity entity, float amount) {
+        setGrowth(min(getGrowth() + amount, getMaxGrowth()));
+        if (getGrowth() >= getMaxGrowth())
+            growUp(entity);
+    }
 
-	LivingEntity growInto();
+    LivingEntity growInto();
 
-	default void growUp(LivingEntity entity) {
-		var world = entity.level();
-		if (!world.isClientSide()) {
-			var newEntity = growInto();
-			if (newEntity == null)
-				return;
-			newEntity.moveTo(entity.blockPosition(), entity.getYRot(), entity.getXRot());
-			world.addFreshEntity(newEntity);
-			entity.remove(Entity.RemovalReason.DISCARDED);
-		}
-	}
+    default void growUp(LivingEntity entity) {
+        var world = entity.level();
+        if (!world.isClientSide()) {
+            var newEntity = growInto();
+            if (newEntity == null)
+                return;
+            newEntity.moveTo(entity.blockPosition(), entity.getYRot(), entity.getXRot());
+            world.addFreshEntity(newEntity);
+            entity.remove(Entity.RemovalReason.DISCARDED);
+        }
+    }
 
-	default float getGrowthNeededUntilGrowUp() {
-		return getMaxGrowth() - getGrowth();
-	}
+    default float getGrowthNeededUntilGrowUp() {
+        return getMaxGrowth() - getGrowth();
+    }
 
-	default float getGrowthMultiplier() {
-		return 1.0f;
-	}
+    default float getGrowthMultiplier() {
+        return 1.0f;
+    }
 }
