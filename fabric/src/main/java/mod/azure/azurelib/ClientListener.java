f1392248d38a61008543244241578b2a7882cb6d
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-11-11 14:13:20 -0500

    Fixes Forge KeyMapping of Reload button

--- fabric/src/main/java/mod/azure/azurelib/ClientListener.java
+++ fabric/src/main/java/mod/azure/azurelib/ClientListener.java
@@ -1,16 +1,20 @@
 package mod.azure.azurelib;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import mod.azure.azurelib.network.Networking;
 import mod.azure.azurelib.platform.Services;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import org.lwjgl.glfw.GLFW;
 
 public final class ClientListener implements ClientModInitializer {
 
-	@Override
-	public void onInitializeClient() {
-		KeyBindingHelper.registerKeyBinding(Keybindings.RELOAD);
-		Services.NETWORK.registerClientReceiverPackets();
+    @Override
+    public void onInitializeClient() {
+        Keybindings.RELOAD = new KeyMapping("key.azurelib.reload", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R, "category.azurelib.binds");
+        KeyBindingHelper.registerKeyBinding(Keybindings.RELOAD);
+        Services.NETWORK.registerClientReceiverPackets();
         Networking.PacketRegistry.registerClient();
-	}
+    }
 }
