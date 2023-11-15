5142f7ebd50a38d86486ee1b7c1d9665a96261ff
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-11-14 21:39:05 -0500

    Add SCOPE keybind for myself

--- common/src/main/java/mod/azure/azurelib/Keybindings.java
+++ common/src/main/java/mod/azure/azurelib/Keybindings.java
@@ -4,6 +4,7 @@ import net.minecraft.client.KeyMapping;
 
 public class Keybindings {
     public static KeyMapping RELOAD;
+    public static KeyMapping SCOPE;
 
     private Keybindings() {
         throw new UnsupportedOperationException();
