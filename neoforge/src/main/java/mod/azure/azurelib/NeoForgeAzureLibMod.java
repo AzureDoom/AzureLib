f2488fcc6246423b1e9ade257aac62b989c9accd
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-11-20 14:08:53 -0500

    RegistryObject -> Supplier

--- neoforge/src/main/java/mod/azure/azurelib/NeoForgeAzureLibMod.java
+++ neoforge/src/main/java/mod/azure/azurelib/NeoForgeAzureLibMod.java
@@ -1,29 +1,21 @@
 package mod.azure.azurelib;
 
-import mod.azure.azurelib.config.ConfigHolder;
 import mod.azure.azurelib.config.TestingConfig;
 import mod.azure.azurelib.config.format.ConfigFormats;
 import mod.azure.azurelib.config.io.ConfigIO;
 import mod.azure.azurelib.entities.TickingLightBlock;
 import mod.azure.azurelib.entities.TickingLightEntity;
 import mod.azure.azurelib.network.Networking;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.ForgeRegistries;
-import net.neoforged.neoforge.registries.RegistryObject;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.function.Supplier;
 
 @Mod(AzureLib.MOD_ID)
 public final class NeoForgeAzureLibMod {
@@ -45,15 +37,15 @@ public final class NeoForgeAzureLibMod {
     }
 
     public class AzureBlocks {
-        public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, AzureLib.MOD_ID);
+        public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, AzureLib.MOD_ID);
 
-        public static final RegistryObject<Block> TICKING_LIGHT_BLOCK = BLOCKS.register("lightblock", () -> new TickingLightBlock());
+        public static final Supplier<TickingLightBlock> TICKING_LIGHT_BLOCK = BLOCKS.register("lightblock", () -> new TickingLightBlock());
     }
 
     public class AzureEntities {
 
-        public static final DeferredRegister<BlockEntityType<?>> TILE_TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, AzureLib.MOD_ID);
+        public static final DeferredRegister<BlockEntityType<?>> TILE_TYPES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, AzureLib.MOD_ID);
 
-        public static final RegistryObject<BlockEntityType<TickingLightEntity>> TICKING_LIGHT_ENTITY = TILE_TYPES.register("lightblock", () -> BlockEntityType.Builder.of(TickingLightEntity::new, AzureBlocks.TICKING_LIGHT_BLOCK.get()).build(null));
+        public static final Supplier<BlockEntityType<TickingLightEntity>> TICKING_LIGHT_ENTITY = TILE_TYPES.register("lightblock", () -> BlockEntityType.Builder.of(TickingLightEntity::new, AzureBlocks.TICKING_LIGHT_BLOCK.get()).build(null));
     }
 }
