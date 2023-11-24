b1e889a839801c91d75b067c73cf9e770fde4ddc
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-11-24 10:43:13 -0500

    Added new Enchantment for various gun mods, Incendiary.

--- fabric/src/main/java/mod/azure/azurelib/FabricAzureLibMod.java
+++ fabric/src/main/java/mod/azure/azurelib/FabricAzureLibMod.java
@@ -3,6 +3,7 @@ package mod.azure.azurelib;
 import mod.azure.azurelib.config.TestingConfig;
 import mod.azure.azurelib.config.format.ConfigFormats;
 import mod.azure.azurelib.config.io.ConfigIO;
+import mod.azure.azurelib.enchantments.IncendiaryEnchantment;
 import mod.azure.azurelib.entities.TickingLightBlock;
 import mod.azure.azurelib.entities.TickingLightEntity;
 import net.fabricmc.api.ModInitializer;
@@ -11,21 +12,25 @@ import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityT
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public final class FabricAzureLibMod implements ModInitializer {
 	public static BlockEntityType<TickingLightEntity> TICKING_LIGHT_ENTITY;
 	public static final TickingLightBlock TICKING_LIGHT_BLOCK = new TickingLightBlock();
+	public static final Enchantment INCENDIARYENCHANTMENT = new IncendiaryEnchantment(Enchantment.Rarity.RARE, EquipmentSlot.MAINHAND);
 
 	@Override
 	public void onInitialize() {
 		ConfigIO.FILE_WATCH_MANAGER.startService();
 		AzureLib.initialize();
-		Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(AzureLib.MOD_ID, "lightblock"), FabricAzureLibMod.TICKING_LIGHT_BLOCK);
+		Registry.register(BuiltInRegistries.BLOCK, AzureLib.modResource("lightblock"), FabricAzureLibMod.TICKING_LIGHT_BLOCK);
 		FabricAzureLibMod.TICKING_LIGHT_ENTITY = Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, AzureLib.MOD_ID + ":lightblock", FabricBlockEntityTypeBuilder.create(TickingLightEntity::new, FabricAzureLibMod.TICKING_LIGHT_BLOCK).build(null));
 		AzureLibMod.config = AzureLibMod.registerConfig(TestingConfig.class, ConfigFormats.json()).getConfigInstance();
 		ServerLifecycleEvents.SERVER_STOPPING.register((server) -> {
 			ConfigIO.FILE_WATCH_MANAGER.stopService();
 		});
+		Registry.register(BuiltInRegistries.ENCHANTMENT, AzureLib.modResource("incendiaryenchantment"), INCENDIARYENCHANTMENT);
 	}
 }
