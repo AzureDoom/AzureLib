0a7f1decb2fce5ed5727fd7a3b1763c089c7d4be
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-12-04 00:13:45 -0500

    Adds firing keybind/config to enable for my mods

--- common/src/main/java/mod/azure/azurelib/Keybindings.java
+++ common/src/main/java/mod/azure/azurelib/Keybindings.java
@@ -5,6 +5,7 @@ import net.minecraft.client.KeyMapping;
 public class Keybindings {
     public static KeyMapping RELOAD;
     public static KeyMapping SCOPE;
+    public static KeyMapping FIRE_WEAPON;
 
     private Keybindings() {
         throw new UnsupportedOperationException();
--- common/src/main/java/mod/azure/azurelib/config/AzureLibConfig.java
+++ common/src/main/java/mod/azure/azurelib/config/AzureLibConfig.java
@@ -7,4 +7,7 @@ public class AzureLibConfig {
     @Configurable
     @Configurable.Synchronized
     public boolean disableOptifineWarning = false;
+    @Configurable
+    @Configurable.Synchronized
+    public boolean useVanillaUseKey = true;
 }
