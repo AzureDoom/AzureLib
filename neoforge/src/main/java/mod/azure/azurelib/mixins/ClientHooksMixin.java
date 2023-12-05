9fe12ee12760f751f21a41fd59f1361ed3b798de
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-12-05 12:49:33 -0500

    Mixin into ClientHooks to apply AzureLib Armor armor model to entities.

---
+++ neoforge/src/main/java/mod/azure/azurelib/mixins/ClientHooksMixin.java
@@ -0,0 +1,24 @@
+package mod.azure.azurelib.mixins;
+
+import mod.azure.azurelib.animatable.GeoItem;
+import mod.azure.azurelib.animatable.client.RenderProvider;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.Model;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.client.ClientHooks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientHooks.class)
+public class ClientHooksMixin {
+
+    @Inject(method = "getArmorModel", at = @At("RETURN"), remap = false, cancellable = true)
+    private static void injectAzureArmors(LivingEntity entityLiving, ItemStack itemStack, EquipmentSlot slot, HumanoidModel<?> _default, CallbackInfoReturnable<Model> cir) {
+        if (itemStack.getItem() instanceof GeoItem)
+            cir.setReturnValue((Model) RenderProvider.of(itemStack).getGenericArmorModel(entityLiving, itemStack, slot, (HumanoidModel<LivingEntity>) _default));
+    }
+}
