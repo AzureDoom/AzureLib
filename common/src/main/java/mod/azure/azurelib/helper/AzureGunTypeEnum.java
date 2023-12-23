9f181f3e7067b88387334e595458214e41c47d3e
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-12-23 13:07:28 -0500

    Backported AzureBaseGunItem

---
+++ common/src/main/java/mod/azure/azurelib/helper/AzureGunTypeEnum.java
@@ -0,0 +1,4 @@
+package mod.azure.azurelib.helper;
+
+public enum AzureGunTypeEnum {
+}
--- common/src/main/java/mod/azure/azurelib/util/AzureLibUtil.java
+++ common/src/main/java/mod/azure/azurelib/util/AzureLibUtil.java
@@ -1,5 +1,11 @@
 package mod.azure.azurelib.util;
 
+import mod.azure.azurelib.entities.TickingLightEntity;
+import mod.azure.azurelib.items.AzureBaseGunItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
 import mod.azure.azurelib.constant.DataTickets;
@@ -116,5 +122,74 @@ public final class AzureLibUtil {
 			if (!entity.hasEffect(effect))
 				areaEffectCloudEntity.addEffect(new MobEffectInstance(effect, effectTime, 0));
 		entity.level().addFreshEntity(areaEffectCloudEntity);
+	}
+
+	/**
+	 * Removes matching item from offhand first then checks inventory for item
+	 *
+	 * @param ammo         Item you want to be used as ammo
+	 * @param playerEntity Player whose inventory is being checked.
+	 */
+	public static void removeAmmo(Item ammo, Player playerEntity) {
+		if ((playerEntity.getItemInHand(
+				playerEntity.getUsedItemHand()).getItem() instanceof AzureBaseGunItem) && !playerEntity.isCreative()) { // Creative mode reloading breaks things
+			for (var item : playerEntity.getInventory().offhand) {
+				if (item.getItem() == ammo) {
+					item.shrink(1);
+					break;
+				}
+				for (var item1 : playerEntity.getInventory().items) {
+					if (item1.getItem() == ammo) {
+						item1.shrink(1);
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Call wherever you are firing weapon to place the half tick light-block, making sure do so only on the server.
+	 *
+	 * @param entity         Usually the player or mob that is using the weapon
+	 * @param isInWaterBlock Checks if it's in a water block to refresh faster.
+	 */
+	public static void spawnLightSource(Entity entity, boolean isInWaterBlock) {
+		BlockPos lightBlockPos = null;
+		if (lightBlockPos == null) {
+			lightBlockPos = AzureLibUtil.findFreeSpace(entity.level(), entity.blockPosition());
+			if (lightBlockPos == null) return;
+			entity.level().setBlockAndUpdate(lightBlockPos,
+					mod.azure.azurelib.platform.Services.PLATFORM.getTickingLightBlock().defaultBlockState());
+		} else if (AzureLibUtil.checkDistance(lightBlockPos, entity.blockPosition()) && entity.level().getBlockEntity(
+				lightBlockPos) instanceof TickingLightEntity tickingLightEntity) {
+			tickingLightEntity.refresh(isInWaterBlock ? 20 : 0);
+		}
+	}
+
+	private static boolean checkDistance(BlockPos blockPosA, BlockPos blockPosB) {
+		return Math.abs(blockPosA.getX() - blockPosB.getX()) <= 2 && Math.abs(
+				blockPosA.getY() - blockPosB.getY()) <= 2 && Math.abs(blockPosA.getZ() - blockPosB.getZ()) <= 2;
+	}
+
+	private static BlockPos findFreeSpace(Level world, BlockPos blockPos) {
+		if (blockPos == null) return null;
+
+		var offsets = new int[2 * 2 + 1];
+		offsets[0] = 0;
+		for (var i = 2; i <= 2 * 2; i += 2) {
+			offsets[i - 1] = i / 2;
+			offsets[i] = -i / 2;
+		}
+		for (var x : offsets)
+			for (var y : offsets)
+				for (var z : offsets) {
+					var offsetPos = blockPos.offset(x, y, z);
+					var state = world.getBlockState(offsetPos);
+					if (state.isAir() || state.getBlock().equals(
+							mod.azure.azurelib.platform.Services.PLATFORM.getTickingLightBlock()))
+						return offsetPos;
+				}
+		return null;
 	}
 }
