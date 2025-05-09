a825e19bfe923a9a3380e1dc6334e9cbf3d667f7
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-05-08 22:56:46 -0400

    Now properly hide player outlayers on equipping armor

--- neo/src/main/java/mod/azure/azurelib/neoforge/mixins/MixinHumanoidArmorLayer.java
+++ neo/src/main/java/mod/azure/azurelib/neoforge/mixins/MixinHumanoidArmorLayer.java
@@ -10,16 +10,21 @@ import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.FastColor;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.DyedItemColor;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -106,8 +111,30 @@ public abstract class MixinHumanoidArmorLayer<T extends LivingEntity, A extends 
 
             renderer.prepForRender(entity, stack, equipmentSlot, baseModel);
             baseModel.copyPropertiesTo(typedHumanoidModel);
+            azurelib$testVisibility((A) typedHumanoidModel, entity, equipmentSlot);
             armorModel.renderToBuffer(poseStack, null, packedLight, OverlayTexture.NO_OVERLAY, i2);
             ci.cancel();
+        }
+    }
+
+    @Unique
+    private void azurelib$testVisibility(A model, @Nullable Entity entity, EquipmentSlot equipmentSlot) {
+        if (entity instanceof Player && model instanceof PlayerModel<?> playerModel) {
+            switch (equipmentSlot) {
+                case HEAD -> {
+                    playerModel.hat.visible = false;
+                    playerModel.ear.visible = false;
+                }
+                case CHEST -> {
+                    playerModel.jacket.visible = false;
+                    playerModel.rightSleeve.visible = false;
+                    playerModel.leftSleeve.visible = false;
+                }
+                case LEGS -> {
+                    playerModel.leftPants.visible = false;
+                    playerModel.rightPants.visible = false;
+                }
+            }
         }
     }
 }
