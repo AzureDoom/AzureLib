06a5eedc1cd1fca4651b73fd0cb62b2859782e98
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-05-16 22:55:14 -0400

    Fixes crash due to Java generics and lambdas

--- Forge/src/main/java/mod/azure/azurelib/rewrite/render/AzRendererConfig.java
+++ Forge/src/main/java/mod/azure/azurelib/rewrite/render/AzRendererConfig.java
@@ -28,9 +28,9 @@ public class AzRendererConfig<T> {
 
     protected final Function<T, RenderType> renderTypeFunction;
 
-    private final UnaryOperator<AzRendererPipelineContext<T>> preRenderEntry;
+    private final Function<AzRendererPipelineContext<T>, AzRendererPipelineContext<T>>  preRenderEntry;
 
-    private final UnaryOperator<AzRendererPipelineContext<T>> postRenderEntry;
+    private final Function<AzRendererPipelineContext<T>, AzRendererPipelineContext<T>>  postRenderEntry;
 
     protected final List<AzRenderLayer<T>> renderLayers;
 
@@ -47,8 +47,8 @@ public class AzRendererConfig<T> {
         Function<T, ResourceLocation> modelLocationProvider,
         Function<T, RenderType> renderTypeFunction,
         List<AzRenderLayer<T>> renderLayers,
-        UnaryOperator<AzRendererPipelineContext<T>> preRenderEntry,
-        UnaryOperator<AzRendererPipelineContext<T>> postRenderEntry,
+        Function<AzRendererPipelineContext<T>, AzRendererPipelineContext<T>>  preRenderEntry,
+        Function<AzRendererPipelineContext<T>, AzRendererPipelineContext<T>>  postRenderEntry,
         Function<T, ResourceLocation> textureLocationProvider,
         Function<T, Float> alphaFunction,
         Function<T, Float> scaleHeight,
@@ -114,9 +114,9 @@ public class AzRendererConfig<T> {
 
         private final List<AzRenderLayer<T>> renderLayers;
 
-        protected UnaryOperator<AzRendererPipelineContext<T>> preRenderEntry;
+        protected Function<AzRendererPipelineContext<T>, AzRendererPipelineContext<T>>  preRenderEntry;
 
-        protected UnaryOperator<AzRendererPipelineContext<T>> postRenderEntry;
+        protected Function<AzRendererPipelineContext<T>, AzRendererPipelineContext<T>>  postRenderEntry;
 
         protected final Function<T, ResourceLocation> textureLocationProvider;
 
@@ -166,14 +166,14 @@ public class AzRendererConfig<T> {
         }
 
         public Builder<T> setPrerenderEntry(
-            UnaryOperator<AzRendererPipelineContext<T>> preRenderEntry
+            Function<AzRendererPipelineContext<T>, AzRendererPipelineContext<T>>  preRenderEntry
         ) {
             this.preRenderEntry = preRenderEntry;
             return this;
         }
 
         public Builder<T> setPostRenderEntry(
-            UnaryOperator<AzRendererPipelineContext<T>> postRenderEntry
+            Function<AzRendererPipelineContext<T>, AzRendererPipelineContext<T>>  postRenderEntry
         ) {
             this.postRenderEntry = postRenderEntry;
             return this;
