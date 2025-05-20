d6c0541592d1abf8e216d06f5b4699e1c6d97da8
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-05-20 19:51:04 -0400

    Resolves an edge crash with comparing two NBTs when grabbing creative menu AzLib items

--- Forge/src/main/java/mod/azure/azurelib/mixin/ItemStackMixin_AzItemStackIdentityRegistry.java
+++ Forge/src/main/java/mod/azure/azurelib/mixin/ItemStackMixin_AzItemStackIdentityRegistry.java
@@ -86,13 +86,9 @@ public class ItemStackMixin_AzItemStackIdentityRegistry {
             return;
         }
 
-        if (!self.hasTag()) {
-            self.setTag(new CompoundTag());
-        }
+        var stackTag = self.getOrCreateTag();
 
-        var stackTag = self.getTag();
-
-        if (stackTag != null && !stackTag.hasUUID(AzureLib.ITEM_UUID_TAG)) {
+        if (!stackTag.hasUUID(AzureLib.ITEM_UUID_TAG)) {
             stackTag.putUUID(AzureLib.ITEM_UUID_TAG, UUID.randomUUID());
         }
     }
