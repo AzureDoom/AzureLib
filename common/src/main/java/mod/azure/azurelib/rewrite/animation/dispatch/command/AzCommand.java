63c78ef75165e08dbb5b3f472b1556f37b9daa2a
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-06-14 16:41:26 -0400

    Item animation packets will now not send itself if the item doesn't animations setup properly via AzIdentityRegistry.

--- common/src/main/java/mod/azure/azurelib/rewrite/animation/dispatch/command/AzCommand.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/animation/dispatch/command/AzCommand.java
@@ -1,5 +1,6 @@
 package mod.azure.azurelib.rewrite.animation.dispatch.command;
 
+import mod.azure.azurelib.rewrite.animation.cache.AzIdentityRegistry;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
@@ -143,6 +144,10 @@ public record AzCommand(List<AzAction> actions) {
         if (entity.level().isClientSide()) {
             dispatchFromClient(entity);
         } else {
+            if (!AzIdentityRegistry.hasIdentity(itemStack.getItem())) {
+                return;
+            }
+
             var uuid = itemStack.getTag().getUUID(AzureLib.ITEM_UUID_TAG);
 
             if (uuid == null) {
