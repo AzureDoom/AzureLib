3e8c1593f5214cba335ee14cc52845434bde8104
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-07-23 00:16:19 -0400

    Adds a ton of new queries, see changelog for all of them

--- Forge/src/main/java/mod/azure/azurelib/rewrite/animation/impl/AzItemAnimator.java
+++ Forge/src/main/java/mod/azure/azurelib/rewrite/animation/impl/AzItemAnimator.java
@@ -1,5 +1,8 @@
 package mod.azure.azurelib.rewrite.animation.impl;
 
+import mod.azure.azurelib.core.molang.MolangParser;
+import mod.azure.azurelib.core.molang.MolangQueries;
+import mod.azure.azurelib.util.RenderUtils;
 import net.minecraft.world.item.ItemStack;
 
 import mod.azure.azurelib.rewrite.animation.AzAnimator;
@@ -22,5 +25,21 @@ public abstract class AzItemAnimator extends AzAnimator<ItemStack> {
 
     protected AzItemAnimator(AzAnimatorConfig config) {
         super(config);
+    }
+
+    @Override
+    protected void applyMolangQueries(ItemStack animatable, double animTime, float partialTicks) {
+        super.applyMolangQueries(animatable, animTime, partialTicks);
+
+        var parser = MolangParser.INSTANCE;
+
+        parser.setMemoizedValue(
+            MolangQueries.ITEM_CURRENT_DURABILITY,
+            () -> animatable.getDamageValue() / (float) animatable.getMaxDamage()
+        );
+        parser.setMemoizedValue(
+            MolangQueries.ITEM_IS_ENCHANTED,
+            () -> RenderUtils.booleanToFloat(!animatable.isEnchanted())
+        );
     }
 }
