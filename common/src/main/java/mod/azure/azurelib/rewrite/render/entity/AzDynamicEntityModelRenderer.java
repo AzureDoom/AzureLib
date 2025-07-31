e5b425f1bbbaab8dae60029c2b72dd8e08111259
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-07-30 22:44:48 -0400

    Fixed an issue using the DynamicModelRenderer, you were required to set a render type for the bone. The RenderType is now properly optional.

--- common/src/main/java/mod/azure/azurelib/rewrite/render/entity/AzDynamicEntityModelRenderer.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/render/entity/AzDynamicEntityModelRenderer.java
@@ -98,11 +98,15 @@ public abstract class AzDynamicEntityModelRenderer<T extends Entity> extends AzE
             context.partialTick()
         );
 
-        if (texture != null && renderTypeOverride == null)
-            renderTypeOverride = renderType;
+        if (texture != null && renderTypeOverride == null) {
+            renderTypeOverride = context.getDefaultRenderType(context.animatable(), texture, context.multiBufferSource(), context.partialTick());
+            renderType = renderTypeOverride;
+        }
 
-        if (renderTypeOverride != null)
+        if (renderTypeOverride != null) {
             context.setVertexConsumer(bufferSource.getBuffer(renderTypeOverride));
+            renderType = renderTypeOverride;
+        }
 
         if (
             !boneRenderOverride(
@@ -118,11 +122,8 @@ public abstract class AzDynamicEntityModelRenderer<T extends Entity> extends AzE
         )
             super.renderCubesOfBone(context, bone);
 
-        if (
-            renderTypeOverride != null && renderType != null && !isReRender && buffer instanceof BufferBuilder builder
-                && !builder.building
-        ) {
-            context.setVertexConsumer(bufferSource.getBuffer(renderTypeOverride));
+        if (!isReRender && buffer instanceof BufferBuilder builder && !builder.building) {
+            context.setVertexConsumer(bufferSource.getBuffer(renderType));
         }
 
         renderCubesOfBone(context, bone);
