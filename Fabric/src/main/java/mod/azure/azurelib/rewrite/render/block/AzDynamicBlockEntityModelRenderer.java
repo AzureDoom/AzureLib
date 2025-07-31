b7035cff284864106af8998cc2449ce1345947ba
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-07-31 00:14:29 -0400

    Fixed an issue using the DynamicModelRenderer, you were required to set a render type for the bone. The RenderType is now properly optional.

--- Fabric/src/main/java/mod/azure/azurelib/rewrite/render/block/AzDynamicBlockEntityModelRenderer.java
+++ Fabric/src/main/java/mod/azure/azurelib/rewrite/render/block/AzDynamicBlockEntityModelRenderer.java
@@ -94,11 +94,15 @@ public abstract class AzDynamicBlockEntityModelRenderer<T extends BlockEntity> e
             context.partialTick()
         );
 
-        if (texture != null && renderTypeOverride == null)
-            renderTypeOverride = renderType;
+        if (texture != null && renderTypeOverride == null) {
+            renderTypeOverride = context.getDefaultRenderType(context.animatable(), texture, context.multiBufferSource(), context.partialTick());
+            renderType = renderTypeOverride;
+        }
 
-        if (renderTypeOverride != null)
+        if (renderTypeOverride != null) {
             context.setVertexConsumer(bufferSource.getBuffer(renderTypeOverride));
+            renderType = renderTypeOverride;
+        }
 
         if (
             !boneRenderOverride(
@@ -117,11 +121,8 @@ public abstract class AzDynamicBlockEntityModelRenderer<T extends BlockEntity> e
         )
             super.renderCubesOfBone(context, bone);
 
-        if (
-            renderTypeOverride != null && renderType != null && !isReRender && buffer instanceof BufferBuilder builder
-                && !builder.building
-        ) {
-            context.setVertexConsumer(bufferSource.getBuffer(renderTypeOverride));
+        if (!isReRender && buffer instanceof BufferBuilder && !((BufferBuilder) buffer).building) {
+            context.setVertexConsumer(bufferSource.getBuffer(renderType));
         }
 
         renderCubesOfBone(context, bone);
--- Fabric/src/main/java/mod/azure/azurelib/rewrite/render/item/AzDynamicItemModelRenderer.java
+++ Fabric/src/main/java/mod/azure/azurelib/rewrite/render/item/AzDynamicItemModelRenderer.java
@@ -83,11 +83,15 @@ public abstract class AzDynamicItemModelRenderer extends AzItemModelRenderer {
             context.partialTick()
         );
 
-        if (texture != null && renderTypeOverride == null)
-            renderTypeOverride = renderType;
+        if (texture != null && renderTypeOverride == null) {
+            renderTypeOverride = context.getDefaultRenderType(context.animatable(), texture, context.multiBufferSource(), context.partialTick());
+            renderType = renderTypeOverride;
+        }
 
-        if (renderTypeOverride != null)
+        if (renderTypeOverride != null) {
             context.setVertexConsumer(bufferSource.getBuffer(renderTypeOverride));
+            renderType = renderTypeOverride;
+        }
 
         if (
             !boneRenderOverride(
@@ -106,11 +110,8 @@ public abstract class AzDynamicItemModelRenderer extends AzItemModelRenderer {
         )
             super.renderCubesOfBone(context, bone);
 
-        if (
-            renderTypeOverride != null && renderType != null && !isReRender && buffer instanceof BufferBuilder builder
-                && !builder.building
-        ) {
-            context.setVertexConsumer(bufferSource.getBuffer(renderTypeOverride));
+        if (!isReRender && buffer instanceof BufferBuilder && !((BufferBuilder) buffer).building) {
+            context.setVertexConsumer(bufferSource.getBuffer(renderType));
         }
 
         super.renderRecursively(context, bone, isReRender);
