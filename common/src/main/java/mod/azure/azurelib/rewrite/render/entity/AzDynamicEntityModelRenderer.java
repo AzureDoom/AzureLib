85e34b359a12413a3cc7e0f330567f3b42d2e273
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-08-03 23:57:10 -0400

    Lint

--- common/src/main/java/mod/azure/azurelib/rewrite/render/entity/AzDynamicEntityModelRenderer.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/render/entity/AzDynamicEntityModelRenderer.java
@@ -99,7 +99,12 @@ public abstract class AzDynamicEntityModelRenderer<T extends Entity> extends AzE
         );
 
         if (texture != null && renderTypeOverride == null) {
-            renderTypeOverride = context.getDefaultRenderType(context.animatable(), texture, context.multiBufferSource(), context.partialTick());
+            renderTypeOverride = context.getDefaultRenderType(
+                context.animatable(),
+                texture,
+                context.multiBufferSource(),
+                context.partialTick()
+            );
             renderType = renderTypeOverride;
         }
 
--- common/src/main/java/mod/azure/azurelib/rewrite/render/item/AzDynamicItemModelRenderer.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/render/item/AzDynamicItemModelRenderer.java
@@ -82,7 +82,12 @@ public abstract class AzDynamicItemModelRenderer extends AzItemModelRenderer {
         );
 
         if (texture != null && renderTypeOverride == null) {
-            renderTypeOverride = context.getDefaultRenderType(context.animatable(), texture, context.multiBufferSource(), context.partialTick());
+            renderTypeOverride = context.getDefaultRenderType(
+                context.animatable(),
+                texture,
+                context.multiBufferSource(),
+                context.partialTick()
+            );
             renderType = renderTypeOverride;
         }
 
