281547b5aa7a276ca0189b3e089e7dcafdf32ed2
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-09-24 00:18:20 -0400

    Fixed AzArmorLayer rotation, now properly rotates armor to entity. Seems to be a bug in only in newer versions of Minecraft where I need to flip the rotation

--- common/src/main/java/mod/azure/azurelib/rewrite/render/layer/AzArmorLayer.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/render/layer/AzArmorLayer.java
@@ -133,7 +133,6 @@ public class AzArmorLayer<T extends LivingEntity> implements AzRenderLayer<T> {
 
         if (!modelPart.cubes.isEmpty()) {
             context.poseStack().pushPose();
-            context.poseStack().scale(-1, -1, 1);
 
             if (renderer != null) {
                 prepModelPartForRender(context, bone, modelPart);
