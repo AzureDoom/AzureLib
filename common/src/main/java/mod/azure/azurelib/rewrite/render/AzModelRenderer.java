7e1b2d6af34fbadd12e4dd99cd517b6942fce911
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-09-29 05:08:11 -0400

    Update AzModelRenderer.java

--- common/src/main/java/mod/azure/azurelib/rewrite/render/AzModelRenderer.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/render/AzModelRenderer.java
@@ -139,8 +139,6 @@ public class AzModelRenderer<T> {
         }
     }
 
-    private final Vector4f poseStateTransformCache = new Vector4f();
-
     /**
      * Applies the {@link GeoQuad Quad's} {@link GeoVertex vertices} to the given {@link VertexConsumer buffer} for
      * rendering
