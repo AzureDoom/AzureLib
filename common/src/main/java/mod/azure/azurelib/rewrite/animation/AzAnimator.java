904602d7ec373bb2322b1d24b470b4268f5714aa
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-09-29 06:42:03 -0400

    Move reusaablecontext to a method to override for AzAnimators

--- common/src/main/java/mod/azure/azurelib/rewrite/animation/AzAnimator.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/animation/AzAnimator.java
@@ -39,8 +39,20 @@ public abstract class AzAnimator<T> {
         var boneCache = new AzBoneCache();
         var timer = new AzAnimationTimer(config);
 
-        this.reusableContext = new AzAnimationContext<>(boneCache, config, timer);
+        this.reusableContext = createReusableContext(config);
     }
+
+	public AzBoneCache createBoneCache() {
+		return new AzBoneCache();
+	}
+
+	public AzAnimationTimer createAzAnimationTimer(AzAnimatorConfig config) {
+		return new AzAnimationTimer(config);
+	}
+
+	public AzAnimationContext<T> createReusableContext(AzAnimatorConfig config) {
+		return new AzAnimationContext<>(createBoneCache(), config, createAzAnimationTimer(config));
+	}
 
     public abstract void registerControllers(AzAnimationControllerContainer<T> animationControllerContainer);
 
