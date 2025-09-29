4d678df4260ccda731f3436d101d44516aa25460
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-09-29 04:15:01 -0400

    Fixed incorrect PoseStack#pop for Armors

--- common/src/main/java/mod/azure/azurelib/rewrite/render/armor/AzArmorModelRenderer.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/render/armor/AzArmorModelRenderer.java
@@ -61,7 +61,7 @@ public class AzArmorModelRenderer extends AzModelRenderer<ItemStack> {
         // TODO: This is dangerous.
         var ctx = armorRendererPipeline.context();
 
-        poseStack.popPose();
+        poseStack.pushPose();
         if (bone.isTrackingMatrices()) {
             Matrix4f poseState = new Matrix4f(poseStack.last().pose());
             Matrix4f localMatrix = RenderUtils.invertAndMultiplyMatrices(
