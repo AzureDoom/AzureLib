caa96910186d6c3791618572c9a51a8116cf41a2
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-01 17:35:54 -0400

    Fixed mixin crash with mods such as Occultism whom have a stupid strict tag check

--- common/src/main/java/mod/azure/azurelib/mixins/ItemStackMixin_AzItemStackIdentityRegistry.java
+++ common/src/main/java/mod/azure/azurelib/mixins/ItemStackMixin_AzItemStackIdentityRegistry.java
@@ -60,9 +60,14 @@ public class ItemStackMixin_AzItemStackIdentityRegistry {
         var self = AzureLibUtil.<ItemStack>self(stackObject);
         var itemRenderer = AzItemRendererRegistry.getOrNull(self.getItem());
         var armorRenderer = AzArmorRendererRegistry.getOrNull(self.getItem());
+
+        // Required due to stupid mods like Occultism and it's stupid strict tags
+        if (itemRenderer == null && armorRenderer == null) {
+            return;
+        }
         var stackTag = self.getOrCreateTag();
 
-        if ((itemRenderer != null || armorRenderer != null) && !stackTag.hasUUID(AzureLib.ITEM_UUID_TAG)) {
+        if (!stackTag.hasUUID(AzureLib.ITEM_UUID_TAG)) {
             stackTag.putUUID(AzureLib.ITEM_UUID_TAG, UUID.randomUUID());
         }
     }
