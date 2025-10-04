65793d08cee482d86c874ec870258ca132124363
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-04 16:08:49 -0400

    Added setShouldAnimateInContext, disableAnimationInContexts, and enableAnimationOnlyInContexts methods to AzItemRendererConfig
    This allows you to disable animations in certain display contexts

--- common/src/main/java/mod/azure/azurelib/rewrite/render/item/AzItemRendererPipelineContext.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/render/item/AzItemRendererPipelineContext.java
@@ -2,6 +2,7 @@ package mod.azure.azurelib.rewrite.render.item;
 
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -21,6 +22,8 @@ public class AzItemRendererPipelineContext extends AzRendererPipelineContext<Ite
 
     private boolean translucent = false;
 
+    private ItemTransforms.TransformType transformType;
+
     public AzItemRendererPipelineContext(AzRendererPipeline<ItemStack> rendererPipeline) {
         super(rendererPipeline);
     }
@@ -34,6 +37,14 @@ public class AzItemRendererPipelineContext extends AzRendererPipelineContext<Ite
      */
     public void setTranslucent(boolean translucent) {
         this.translucent = translucent;
+    }
+
+    public ItemTransforms.TransformType getTransformType() {
+        return transformType;
+    }
+
+    public void setTransformType(ItemTransforms.TransformType transformType) {
+        this.transformType = transformType;
     }
 
     @Override
