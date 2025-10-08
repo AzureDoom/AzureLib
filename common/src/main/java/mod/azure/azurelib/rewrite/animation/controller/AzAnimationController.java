74e0fc6745188ed965b8f51bcc4ac0e8c9f7ed7e
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-08 14:49:31 -0400

    Fixes when an invalid animations name is used, the controller/state machine will become stuck and no longer update

--- common/src/main/java/mod/azure/azurelib/rewrite/animation/controller/AzAnimationController.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/animation/controller/AzAnimationController.java
@@ -194,7 +194,9 @@ public class AzAnimationController<T> extends AzAbstractAnimationController {
                 return;
             }
 
-            stateMachine.stop();
+	        animationQueue.clear();
+	        this.currentSequence = null;
+	        stateMachine.transition();
         }
     }
 
