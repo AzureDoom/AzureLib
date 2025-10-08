64df0606dd44deb0f33bdf4180def8d605ea62ad
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-08 12:04:49 -0400

    Fixed builder config render type config completely overwriting the default render type setups

--- common/src/main/java/mod/azure/azurelib/rewrite/render/AzRendererPipeline.java
+++ common/src/main/java/mod/azure/azurelib/rewrite/render/AzRendererPipeline.java
@@ -92,7 +92,14 @@ public abstract class AzRendererPipeline<T> implements AzPhasedRenderer<T> {
         float partialTick,
         int packedLight
     ) {
-        renderType = config.getRenderType(animatable);
+        renderType = context.getDefaultRenderType(
+            animatable,
+            config.textureLocation(animatable),
+            bufferSource,
+            partialTick,
+            config.getRenderType(animatable),
+            config.alpha(animatable)
+        );
         context.populate(animatable, model, bufferSource, packedLight, partialTick, poseStack, renderType, buffer);
 
         poseStack.pushPose();
