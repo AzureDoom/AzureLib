f67fbce1a0d568a1f9b95fb130f985eada2e026b
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-10 17:35:15 -0400

    Fixes enableAnimationOnlyInContexts not working properly

--- common/src/main/java/mod/azure/azurelib/render/item/AzItemRendererConfig.java
+++ common/src/main/java/mod/azure/azurelib/render/item/AzItemRendererConfig.java
@@ -298,7 +298,7 @@ public class AzItemRendererConfig extends AzRendererConfig<ItemStack> {
             }
 
             var finalEnabledContexts = Set.copyOf(enabledContexts);
-            this.shouldAnimateInContext = context -> !finalEnabledContexts.contains(context);
+            this.shouldAnimateInContext = finalEnabledContexts::contains;
             return this;
         }
 
