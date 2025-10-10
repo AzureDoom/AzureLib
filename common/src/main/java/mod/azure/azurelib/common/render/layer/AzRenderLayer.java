fe268f34dbc5e255dfe5de97f521d60e9835f03a
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2025-10-10 17:15:01 -0400

    Fixes Java Docs

--- common/src/main/java/mod/azure/azurelib/common/render/layer/AzRenderLayer.java
+++ common/src/main/java/mod/azure/azurelib/common/render/layer/AzRenderLayer.java
@@ -9,10 +9,10 @@ import mod.azure.azurelib.common.render.AzRendererPipelineContext;
 
 /**
  * Render layer base class for rendering additional layers of effects or textures over an existing model at runtime.<br>
- * </p>
+ * <p>
  * This interface defines the lifecycle hooks used by the rendering pipeline to apply custom visual effects, overlays,
  * or transformations on top of the base model.<br>
- * </p>
+ * <p>
  *
  * @param <K> The type of the key used to identify the animatable object. Typically, a UUID for items/entities and Long
  *            for BlockEntities.
@@ -24,7 +24,7 @@ public interface AzRenderLayer<K, T> {
     /**
      * Called by the {@link AzRendererPipeline} before rendering begins, immediately after
      * {@link AzRendererPipeline#preRender}.
-     * </p>
+     * <p>
      * Allows render layers to perform setup tasks such as hiding/showing bones or other pre-render state modifications.
      *
      * @param context The active renderer context containing pipeline state, the animatable instance, and the associated
@@ -35,7 +35,7 @@ public interface AzRenderLayer<K, T> {
     /**
      * Called by the renderer after the main animatable has been rendered, but before supplementary features (like name
      * tags).
-     * </p>
+     * <p>
      * This is the primary method to implement custom render layer logic.
      *
      * @param context The active renderer context containing pipeline state, the animatable instance, and the associated
@@ -45,12 +45,12 @@ public interface AzRenderLayer<K, T> {
 
     /**
      * Called by the {@link AzRendererPipeline} for each {@link AzBone} being rendered.<br>
-     * </p>
+     * <p>
      * This is more expensive than {@link #render} since it occurs per-bone, but it has the advantage of having all
      * matrix transformations already applied.<br>
-     * </p>
+     * <p>
      * The {@link AzBone} in question has already been rendered by this stage.<br>
-     * </p>
+     * <p>
      * <b>Important:</b> If you modify the {@link VertexConsumer buffer}, reset it to the previous one via
      * {@link MultiBufferSource#getBuffer} before returning.
      *
