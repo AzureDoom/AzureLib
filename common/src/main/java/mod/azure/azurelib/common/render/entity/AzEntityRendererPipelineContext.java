ed71dc1ec0950bf753be7c8bbdce38a400aed986
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2025-10-19 21:26:42 -0400

    Tweak to Fixed quads disappearing when entity gets hurt

--- common/src/main/java/mod/azure/azurelib/common/render/entity/AzEntityRendererPipelineContext.java
+++ common/src/main/java/mod/azure/azurelib/common/render/entity/AzEntityRendererPipelineContext.java
@@ -44,6 +44,12 @@ public class AzEntityRendererPipelineContext<T extends Entity> extends AzRendere
 
         // Handle entity damage/death state
         if (visibleBody && !glowing && hurtOrDead) {
+            if (
+                defaultRenderType == RenderType.entityTranslucentCull(texture) || defaultRenderType == RenderType
+                    .entityTranslucent(texture)
+            ) {
+                return RenderType.entityCutoutNoCull(texture);
+            }
             return defaultRenderType;
         }
 
