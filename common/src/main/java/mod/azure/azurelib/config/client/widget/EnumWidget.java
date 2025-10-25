29d0adca88494d2415cf817555c60d6fa61bad8a
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2025-10-25 12:42:15 -0400

    fixes enum coloring

--- common/src/main/java/mod/azure/azurelib/config/client/widget/EnumWidget.java
+++ common/src/main/java/mod/azure/azurelib/config/client/widget/EnumWidget.java
@@ -46,7 +46,8 @@ public class EnumWidget<E extends Enum<E>> extends AbstractWidget {
             this.getTextureY()
         );
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        this.renderString(graphics, minecraft.font, Mth.ceil(this.alpha * 255.0F) << 24);
+        int i = this.active ? 0xffffff : 0xa0a0a0;
+        this.renderString(graphics, minecraft.font, i | Mth.ceil(this.alpha * 255.0F) << 24);
     }
 
     private int getTextureY() {
