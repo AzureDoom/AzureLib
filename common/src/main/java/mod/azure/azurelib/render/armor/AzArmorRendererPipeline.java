42951c77c6761009f5a10a9d81a1b3a7d9e7cbcd
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2025-10-30 12:02:25 -0400

    Fixed Armor scaling on baby mobs

--- common/src/main/java/mod/azure/azurelib/render/armor/AzArmorRendererPipeline.java
+++ common/src/main/java/mod/azure/azurelib/render/armor/AzArmorRendererPipeline.java
@@ -1,8 +1,8 @@
 package mod.azure.azurelib.render.armor;
 
 import com.mojang.math.Matrix4f;
-import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 
 import java.util.UUID;
@@ -95,7 +95,7 @@ public class AzArmorRendererPipeline extends AzRendererPipeline<UUID, ItemStack>
      */
     public void scaleModelForBaby(AzArmorRendererPipelineContext context, boolean isReRender) {
         var currentEntity = context.currentEntity();
-        if (!(currentEntity instanceof AgeableMob ageableMob && ageableMob.isBaby()) || isReRender) {
+        if (!(currentEntity instanceof LivingEntity ageableMob && ageableMob.isBaby()) || isReRender) {
             return;
         }
 
