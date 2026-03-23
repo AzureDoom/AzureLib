cac9e5cc0b35c47ec08cbcd50c36ebac76144f8f
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-03-22 20:30:57 -0400

    Fixes animated texture mixin conflict with Gankolib

--- common/src/main/java/mod/azure/azurelib/common/internal/mixins/TextureManagerMixin.java
+++ common/src/main/java/mod/azure/azurelib/common/internal/mixins/TextureManagerMixin.java
@@ -6,43 +6,46 @@
 package mod.azure.azurelib.common.internal.mixins;
 
 import net.minecraft.client.renderer.texture.AbstractTexture;
+import net.minecraft.client.renderer.texture.SimpleTexture;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Map;
-
 import mod.azure.azurelib.common.cache.texture.AnimatableTexture;
 
-@Mixin(value = TextureManager.class, priority = 900)
+@Mixin(value = TextureManager.class, priority = 2010)
 public abstract class TextureManagerMixin {
-
-    @Shadow
-    @Final
-    private Map<ResourceLocation, AbstractTexture> byPath;
 
     @Shadow
     public abstract void register(ResourceLocation resourceLocation, AbstractTexture abstractTexture);
 
     @Inject(
         method = "getTexture(Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/client/renderer/texture/AbstractTexture;",
-        at = @At("HEAD")
+        at = @At("RETURN"),
+        cancellable = true,
+        require = 0
     )
-    private void wrapAnimatableTexture(ResourceLocation path, CallbackInfoReturnable<AbstractTexture> callback) {
-        AbstractTexture existing = this.byPath.get(path);
+    private void azurelib$replaceAnimatableTexture(
+        ResourceLocation location,
+        CallbackInfoReturnable<AbstractTexture> cir
+    ) {
+        AbstractTexture currentTexture = cir.getReturnValue();
 
-        if (existing == null) {
-            AnimatableTexture animatableTexture = new AnimatableTexture(path);
-
-            register(path, animatableTexture);
-
-            if (!animatableTexture.isAnimated())
-                this.byPath.remove(path);
+        if (currentTexture == null || currentTexture.getClass() != SimpleTexture.class) {
+            return;
         }
+
+        AnimatableTexture animatableTexture = new AnimatableTexture(location);
+
+        if (!animatableTexture.isAnimated()) {
+            return;
+        }
+
+        this.register(location, animatableTexture);
+        cir.setReturnValue(animatableTexture);
     }
 }
