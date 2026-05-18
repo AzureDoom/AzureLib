d0777dc941f3084f2335a82eb01e1fd3260c47ba
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-05-18 00:24:02 -0400

    Prevent MolangVariableHolder NPE for constant fallbacks

--- common/src/main/java/mod/azure/azurelib/core/molang/expressions/MolangVariableHolder.java
+++ common/src/main/java/mod/azure/azurelib/core/molang/expressions/MolangVariableHolder.java
@@ -5,6 +5,7 @@
  */
 package mod.azure.azurelib.core.molang.expressions;
 
+import mod.azure.azurelib.AzureLib;
 import mod.azure.azurelib.core.math.IValue;
 import mod.azure.azurelib.core.math.Variable;
 
@@ -23,10 +24,14 @@ public class MolangVariableHolder extends MolangValue {
 
     @Override
     public double get() {
-        double value = super.get();
+        var value = super.get();
+
+        if (this.variable == null) {
+            AzureLib.LOGGER.error("Variable is null, defaulting to 0");
+            return 0;
+        }
 
         this.variable.set(value);
-
         return value;
     }
 
