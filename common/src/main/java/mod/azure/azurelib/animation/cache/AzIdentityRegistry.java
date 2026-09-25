664d7b5b0eadb1d79db8045251daf6e5b4aa9df2
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-09-24 23:18:27 -0400

    Update AzIdentityRegistry.java

--- common/src/main/java/mod/azure/azurelib/animation/cache/AzIdentityRegistry.java
+++ common/src/main/java/mod/azure/azurelib/animation/cache/AzIdentityRegistry.java
@@ -4,7 +4,6 @@ import net.minecraft.world.item.Item;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
