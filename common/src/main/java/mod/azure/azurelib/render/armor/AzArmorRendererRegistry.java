f842ae179541ad6dbf15c2e07a70f6880a07f226
AzureDoom <7415711+AzureDoom@users.noreply.github.com>
2026-09-24 22:50:15 -0400

    backport ConcurrentHashMap fix on render registries

--- common/src/main/java/mod/azure/azurelib/render/armor/AzArmorRendererRegistry.java
+++ common/src/main/java/mod/azure/azurelib/render/armor/AzArmorRendererRegistry.java
@@ -3,15 +3,15 @@ package mod.azure.azurelib.render.armor;
 import net.minecraft.world.item.Item;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 public class AzArmorRendererRegistry {
 
-    private static final Map<Item, AzArmorRenderer> ITEM_TO_RENDERER = new HashMap<>();
+    private static final Map<Item, AzArmorRenderer> ITEM_TO_RENDERER = new ConcurrentHashMap<>();
 
-    private static final Map<Item, Supplier<AzArmorRenderer>> ITEM_TO_RENDERER_SUPPLIER = new HashMap<>();
+    private static final Map<Item, Supplier<AzArmorRenderer>> ITEM_TO_RENDERER_SUPPLIER = new ConcurrentHashMap<>();
 
     public static void register(Item item, Supplier<AzArmorRenderer> armorRendererSupplier) {
         ITEM_TO_RENDERER_SUPPLIER.put(item, armorRendererSupplier);
